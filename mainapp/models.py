ee89d129fab6417f42b392b51fa11bcab99065a7
“soniatan” <sonnyeah@gmail.com>
2021-11-18 11:32:27 +0800

    updated Product model, and added base code for a Comment model

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,7 +4,19 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
+    image = models.TextField()
+    description = models.TextField()
+    ingredients = models.TextField()
     price = models.IntegerField()
+    # add comments here?
 
     def __str__(self):
-        return self.brand + " " + self.name
+        return self.brand + " - " + self.name
+
+class Comment(models.Model):
+    username = models.CharField(max_length=100)
+    date = models.DateField()
+    text = models.TextField()
+
+    def __str__(self):
+        return "[" + self.date + "] " + self.username
