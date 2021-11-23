08b8a6da9e72c53ffef5f5d3842b2c7a69255bab
“soniatan” <sonnyeah@gmail.com>
2021-11-23 14:30:26 +0800

    added comment model in backend and modified product model with slug field

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.core.validators import MinValueValidator, MaxValueValidator
+from user.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -8,15 +11,17 @@ class Product(models.Model):
     description = models.TextField()
     ingredients = models.TextField()
     price = models.IntegerField()
-    # add comments here?
+    slug = models.SlugField(max_length=250, null=True)
 
     def __str__(self):
         return self.brand + " - " + self.name
 
 class Comment(models.Model):
-    username = models.CharField(max_length=100)
-    date = models.DateField(auto_now_add=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name="comments")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=None, related_name="comments")
+    rating = models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    created_on = models.DateField(auto_now_add=True)
     text = models.TextField()
 
     def __str__(self):
-        return "[" + self.date + "] " + self.username
+        return "[" + str(self.created_on) + "] " + self.username.username
