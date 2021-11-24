9d82c9b4d5f8677bd6ef413e6fc3c2bd0b64d0e2
soniasltan <sonnyeah@gmail.com>
2021-11-24 16:28:44 +0800

    test commit

--- user/views.py
+++ user/views.py
@@ -5,7 +5,7 @@ from .serializers import CreateUserSerializer
 from .models import User
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.decorators import api_view, permission_classes
+# from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 
